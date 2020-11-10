bfe42171cc8eae543c0992186e24854c860cbb0d
Manish Johari <joharimani@gmail.com>
2020-11-10 11:21:55 +0530

    Implement Action cable with guest User(User.first) # Working.

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,14 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :comments, dependent: :destroy
+
+  validates_presence_of :name
+
+  def first_name
+  	name.split.first
+  end
+
+  def last_name
+  	name.split.last
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,8 @@ Rails.application.routes.draw do
   get 'pages/about'
 
   resources :blogs
+
+  mount ActionCable.server => '/cable'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'pages#home'
 end
